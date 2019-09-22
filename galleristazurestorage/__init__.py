9b559544d90f3a6f83594d7407a933b5a6b21587
Roberto Prevato <roberto.prevato@gmail.com>
2019-09-22 10:39:27 +0200

    handles mime

--- galleristazurestorage/__init__.py
+++ galleristazurestorage/__init__.py
@@ -1,5 +1,6 @@
-from gallerist.abc import SyncFileStore
+from gallerist.abc import SyncFileStore, FileInfo
 from azure.storage.common import CloudStorageAccount
+from azure.storage.blob import ContentSettings
 
 
 class AzureBlobFileStore(SyncFileStore):
@@ -20,8 +21,11 @@ class AzureBlobFileStore(SyncFileStore):
         blob = self.service.get_blob_to_bytes(self.container_name, file_path)
         return blob.content
 
-    def write_file(self, file_path: str, data: bytes):
-        self.service.create_blob_from_bytes(self.container_name, file_path, data)
+    def write_file(self, file_path: str, data: bytes, info: FileInfo):
+        self.service.create_blob_from_bytes(self.container_name,
+                                            file_path,
+                                            data,
+                                            content_settings=ContentSettings(content_type=info.mime))
 
     def delete_file(self, file_path: str):
         self.service.delete_blob(self.container_name, file_path)
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='gallerist-azurestorage',
-      version='0.0.2',
+      version='0.0.3',
       description='Azure Storage file store for Gallerist',
       long_description=readme(),
       long_description_content_type='text/markdown',
